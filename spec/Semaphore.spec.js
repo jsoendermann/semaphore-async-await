6b19b065c7911571be3daf5f967cb17376e55fe7
Jan Soendermann <jan.soendermann+git@gmail.com>
2016-11-04 00:59:12 +0100

    Initial commit

---
+++ spec/Semaphore.spec.js
@@ -0,0 +1,151 @@
+/* global describe, it, expect */
+/* eslint prefer-arrow-callback:0, func-names:0, global-require:0, import/no-extraneous-dependencies:0 */
+
+import install from 'jasmine-es6';
+
+install();
+
+
+const wait = (ms) => new Promise(r => setTimeout(r, ms));
+
+describe('Semaphore', function () {
+  const Semaphore = require('../lib/Semaphore').default;
+
+  it('without semaphore', async function () {
+    let global = 0;
+
+    const f = async () => {
+      const local = global;
+      await wait(500);
+      global = local + 1;
+    };
+
+    f();
+    f();
+
+    await wait(1000);
+
+    expect(global).toEqual(1);
+  });
+
+  it('with semaphore', async function () {
+    let global = 0;
+    const lock = new Semaphore(1);
+
+    const f = async () => {
+      await lock.wait();
+      const local = global;
+      await wait(500);
+      global = local + 1;
+      lock.signal();
+    };
+
+    f();
+    f();
+
+    await wait(1500);
+
+    expect(global).toEqual(2);
+  });
+
+  it('with execute', async function () {
+    let global = 0;
+    const lock = new Semaphore(1);
+
+    const f = async () => {
+      const local = global;
+      await wait(500);
+      global = local + 1;
+    };
+
+    lock.execute(f);
+    lock.execute(f);
+
+    await wait(1500);
+
+    expect(global).toEqual(2);
+  });
+
+  it('with a negative number of initial permits', async function () {
+    const sem = new Semaphore(-2);
+    let global = 0;
+
+    (async () => {
+      await sem.wait();
+      expect(global).toEqual(3);
+    })();
+
+    setTimeout(() => {
+      global += 1;
+      sem.signal();
+      setTimeout(() => {
+        global += 1;
+        sem.signal();
+        setTimeout(() => {
+          global += 1;
+          sem.signal();
+          setTimeout(() => {
+            global += 1;
+            sem.signal();
+          }, 0);
+        }, 0);
+      }, 0);
+    }, 0);
+  });
+
+  it('using waitFor successfully', async function () {
+    const sem = new Semaphore(0);
+
+    (async () => {
+      const didAcquire = await sem.waitFor(1000);
+      expect(didAcquire).toBeTruthy();
+    })();
+
+    await wait(500);
+    sem.signal();
+  });
+
+  it('using waitFor unsuccessfully', async function () {
+    const sem = new Semaphore(0);
+
+    (async () => {
+      const didAcquire = await sem.waitFor(1000);
+      expect(didAcquire).toBeFalsy();
+    })();
+
+    await wait(1500);
+    sem.signal();
+  });
+
+  it('tryAcquire successfully', async function () {
+    const sem = new Semaphore(1);
+    expect(sem.tryAcquire()).toBeTruthy();
+  });
+
+  it('tryAcquire unsuccessfully', async function () {
+    const sem = new Semaphore(0);
+    expect(sem.tryAcquire()).toBeFalsy();
+  });
+
+  it('wait after failed waitFor', async function () {
+    const sem = new Semaphore(1);
+    let global = 0;
+
+    const f = async () => {
+      await sem.wait();
+      const local = global;
+      await wait(500);
+      global = local + 1;
+      sem.signal();
+    };
+
+    f();
+    const waitSuccessful = await sem.waitFor(100);
+    expect(waitSuccessful).toBeFalsy();
+    f();
+
+    await wait(1500);
+
+    expect(global).toEqual(2);
+  });
+});
---
+++ src/Semaphore.js
@@ -0,0 +1,97 @@
+/** Class representing a semaphore */
+export default class Semaphore {
+  /**
+   * Creates a semaphore.
+   * @param {number} permits The number of permits, i.e. things being allowed to run in parallel.
+   * To create a lock that only lets one thing run at a time, set this to 1. This number can also be negative.
+   */
+  constructor(permits) {
+    this._permits = permits;
+
+    this._promiseResolvers = [];
+  }
+
+  /**
+   * Returns a promise used to wait for a permit to become available.
+   * @return {Promise} A promise that gets resolved when execution is allowed to proceed.
+   */
+  async wait() {
+    if (this._permits > 0) {
+      this._permits -= 1;
+      return Promise.resolve(true);
+    }
+    return new Promise(resolver => this._promiseResolvers.push(resolver));
+  }
+
+  /**
+   * Same as wait except the promise returned gets resolved with <code>false</code> if no permit becomes available in time.
+   * @param {number} milliseconds The time spent waiting before the wait is aborted.
+   * @return {Promise} A promise that gets resolved with <code>true</code> when execution is allowed to proceed or
+   * <code>false</code> if the time given elapses before a permit becomes available.
+   */
+  async waitFor(milliseconds) {
+    if (this._permits > 0) {
+      this._permits -= 1;
+      return Promise.resolve(true);
+    }
+
+    let resolver;
+    const promise = new Promise(r => {
+      resolver = r;
+    });
+
+    this._promiseResolvers.push(resolver);
+
+    setTimeout(() => {
+      const index = this._promiseResolvers.indexOf(resolver);
+      if (index !== -1) {
+        this._promiseResolvers.splice(index, 1);
+      }
+      resolver(false);
+    }, milliseconds);
+
+    return promise;
+  }
+
+  /**
+   * Synchronous function used to determine whether a permit is available.
+   * @return {boolean} True if there's at least one permit available.
+   */
+  tryAcquire() {
+    if (this._permits > 0) {
+      this._permits -= 1;
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Increases the number of permits by one. If there are other functions waiting, one of them will
+   * continue to execute in a future iteration of the event loop.
+   */
+  signal() {
+    this._permits += 1;
+
+    if (this._permits >= 1 && this._promiseResolvers.length > 0) {
+      if (this._permits > 1) {
+        throw new Error('this._permits should never be > 0 when there is someone waiting.');
+      }
+
+      this._permits -= 1;
+      this._promiseResolvers.shift()(true);
+    }
+  }
+
+  /**
+   * Schedules func to be called once a permit becomes available.
+   * @param {function} func - The function to be executed.
+   */
+  async execute(func) {
+    await this.wait();
+    try {
+      await func();
+    } finally {
+      this.signal();
+    }
+  }
+}
