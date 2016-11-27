73561f300a1f21f253885605e88e249a97a751e0
Jan Soendermann <jan.soendermann+git@gmail.com>
2016-11-27 02:46:21 +0100

    Port to typescript

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const babel = require('gulp-babel');
+const merge = require('merge2');
+
+
+gulp.task('default', () => {
+  const tsProject = ts.createProject('./tsconfig.json');
+  const babelrc = require('./.babelrc.json');
+
+  const tsStream = tsProject.src()
+    .pipe(tsProject());
+
+  return merge([
+    tsStream
+      .js
+      .pipe(babel(babelrc))
+      .pipe(gulp.dest('lib')),
+    tsStream
+      .dts
+      .pipe(gulp.dest('lib')),
+  ]);
+});
+
+
+gulp.watch('src/**/*.ts', ['default']);
--- src/Semaphore.ts
+++ src/Semaphore.ts
@@ -1,26 +1,24 @@
 /** Class representing a semaphore */
 export default class Semaphore {
+  private promiseResolvers: Array<(v: boolean) => void>;
+
   /**
    * Creates a semaphore.
    * @param {number} permits The number of permits, i.e. things being allowed to run in parallel.
    * To create a lock that only lets one thing run at a time, set this to 1. This number can also be negative.
    */
-  constructor(permits) {
-    this._permits = permits;
-
-    this._promiseResolvers = [];
-  }
+  constructor(private permits: number) {}
 
   /**
    * Returns a promise used to wait for a permit to become available.
    * @return {Promise} A promise that gets resolved when execution is allowed to proceed.
    */
-  async wait() {
-    if (this._permits > 0) {
-      this._permits -= 1;
+  async wait(): Promise<boolean> {
+    if (this.permits > 0) {
+      this.permits -= 1;
       return Promise.resolve(true);
     }
-    return new Promise(resolver => this._promiseResolvers.push(resolver));
+    return new Promise<boolean>(resolver => this.promiseResolvers.push(resolver));
   }
 
   /**
@@ -29,23 +27,23 @@ export default class Semaphore {
    * @return {Promise} A promise that gets resolved with true when execution is allowed to proceed or
    * false if the time given elapses before a permit becomes available.
    */
-  async waitFor(milliseconds) {
-    if (this._permits > 0) {
-      this._permits -= 1;
+  async waitFor(milliseconds: number): Promise<boolean> {
+    if (this.permits > 0) {
+      this.permits -= 1;
       return Promise.resolve(true);
     }
 
-    let resolver;
-    const promise = new Promise(r => {
+    let resolver: (v: boolean) => void;
+    const promise = new Promise<boolean>(r => {
       resolver = r;
     });
 
-    this._promiseResolvers.push(resolver);
+    this.promiseResolvers.push(resolver);
 
     setTimeout(() => {
-      const index = this._promiseResolvers.indexOf(resolver);
+      const index = this.promiseResolvers.indexOf(resolver);
       if (index !== -1) {
-        this._promiseResolvers.splice(index, 1);
+        this.promiseResolvers.splice(index, 1);
       }
       resolver(false);
     }, milliseconds);
@@ -57,9 +55,9 @@ export default class Semaphore {
    * Synchronous function that tries to acquire a permit and returns true if successful, false otherwise.
    * @return {boolean} Whether a permit could be acquired.
    */
-  tryAcquire() {
-    if (this._permits > 0) {
-      this._permits -= 1;
+  tryAcquire(): boolean {
+    if (this.permits > 0) {
+      this.permits -= 1;
       return true;
     }
     return false;
@@ -69,16 +67,16 @@ export default class Semaphore {
    * Increases the number of permits by one. If there are other functions waiting, one of them will
    * continue to execute in a future iteration of the event loop.
    */
-  signal() {
-    this._permits += 1;
+  signal(): void {
+    this.permits += 1;
 
-    if (this._permits >= 1 && this._promiseResolvers.length > 0) {
-      if (this._permits > 1) {
+    if (this.permits >= 1 && this.promiseResolvers.length > 0) {
+      if (this.permits > 1) {
         throw new Error('this._permits should never be > 0 when there is someone waiting.');
       }
 
-      this._permits -= 1;
-      this._promiseResolvers.shift()(true);
+      this.permits -= 1;
+      this.promiseResolvers.shift()(true);
     }
   }
 
@@ -86,7 +84,7 @@ export default class Semaphore {
    * Schedules func to be called once a permit becomes available.
    * @param {function} func - The function to be executed.
    */
-  async execute(func) {
+  async execute(func: () => void) {
     await this.wait();
     try {
       await func();
