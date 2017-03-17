29d060ca73e21c749e630cc3ee130912313fe58e
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-03-17 20:50:05 +0800

    Fix typo in comment

--- src/Lock.ts
+++ src/Lock.ts
@@ -2,7 +2,7 @@ import Semaphore from './Semaphore';
 
 
 /**
- * A lock that can be used to synchronize critical sectionds in your code.
+ * A lock that can be used to synchronize critical sections in your code.
  * For more details on how to use this class, please view the documentation
  * of the Semaphore class from which Lock inherits.
  */
